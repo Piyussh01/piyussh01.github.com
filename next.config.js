04f9adfe896b85d21868603eec813465e56bd3ab
techsavvy <piyusshsinghal01@gmail.com>
2023-12-25 14:44:26 -0700

    Test

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    distDir: 'build',
+    basePath: '/piyussh01.github.com', // Replace with your GitHub repository name
+    assetPrefix: '/piyussh01.github.com/', // Replace with your GitHub repository name
+  }
